d635677ed0df01ab366c5d4ccba0f7160f61ee4e
Marcelo Araujo <marcelocardoso020304@gmail.com>
2023-08-21 10:54:40 -0300

    iniciado o projeto

---
+++ projeto/Filmes_DML.sql
@@ -0,0 +1,7 @@
+USE Filmes;
+
+INSERT INTO Genero(Nome)
+VALUES ('Aventura'), ('Terror')
+
+INSERT INTO Filme(IdGenero,Titulo)
+VALUES (1,'Indiana Jones'),(2,'Evil Dead')
---
+++ projeto/webapi.filme.manha/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,20 @@
+﻿using webapi.filme.manha.Domains;
+
+namespace webapi.filme.manha.Interfaces
+{
+    /// <summary>
+    /// Interface responsavel pelo repositorio GeneroRepository
+    /// Definir os metodos que serao implementados pelo GeneroRepository
+    /// </summary>
+    public interface IGeneroRepository
+    {
+        //TipoRetorno NomeMetodo(TipoParametro NomeParametro)
+
+        void Cadastrar(GeneroDomain novoGenero);
+        List<GeneroDomain> ListarTodos();
+
+        void AtualizarIdCorpo(GeneroDomain genero);
+
+        void Deletar(int id);
+    }
+}
