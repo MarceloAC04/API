f8045a18ebc73593f989156a5ea9474ee4ea0388
Marcelo Araujo <marcelocardoso020304@gmail.com>
2023-08-23 10:48:15 -0300

    criada a controller

--- projeto/webapi.filme.manha/Program.cs
+++ projeto/webapi.filme.manha/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Adiciona serviço de  Controller
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+//Adiciona o mapeamento dos controllers
+app.MapControllers();
+
+app.UseHttpsRedirection();
 
 app.Run();
--- projeto/webapi.filme.manha/Repositories/GeneroRepository.cs
+++ projeto/webapi.filme.manha/Repositories/GeneroRepository.cs
@@ -1,4 +1,5 @@
-﻿using webapi.filme.manha.Domains;
+﻿using System.Data.SqlClient;
+using webapi.filme.manha.Domains;
 using webapi.filme.manha.Interfaces;
 
 namespace webapi.filme.manha.Repositories
@@ -13,8 +14,9 @@ namespace webapi.filme.manha.Repositories
         ///     -Windows : Intergrated Security = true
         ///     -SQLServer : User Id = sa Pwd = Senha
         /// </summary>
-        private string StringConexao = "Data Source = NOTE23-S15; Initial Catalog = Filmes; Integrated Security = true";
-                                                                                            // User Id = sa; Pwd = Senha";
+        private string StringConexao = "Data Source = NOTE23-S15; Initial Catalog = Filmes; User Id = sa; Pwd = Senai@134";
+                                                                                            // Integrated Security = true";
+                                                                                         
         public void AtualizarIdCorpo(GeneroDomain genero)
         {
             throw new NotImplementedException();
@@ -40,9 +42,51 @@ namespace webapi.filme.manha.Repositories
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Listar todos os obejtos generos
+        /// </summary>
+        /// <returns>Lista de objetos (generos)</returns>
         public List<GeneroDomain> ListarTodos()
         {
-            throw new NotImplementedException();
+            //Cria uma lista de objetos tipo genero
+            List<GeneroDomain> listaGeneros = new List<GeneroDomain>();
+
+            //Declara a SqlConnection passando a string de conexao como parametro
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                 //Declara a instrução a ser executada
+                 string querySelectAll  = "SELECT IdGenero, Nome FROM Genero";
+
+                 //Abre a conexao com o banco de dados
+                 con.Open();
+
+                //Declara o SqlDataReader para percorrer a tabela do banco
+                SqlDataReader rdr;
+
+                //Declara o SqlComand passando a query que sera executada e a conexao com o banco
+                using (SqlCommand cmd = new SqlCommand(querySelectAll, con))
+                {
+                    //Executa a query e armazena os dados no rdr
+                    rdr = cmd.ExecuteReader();
+
+                    while(rdr.Read())
+                    {
+                        GeneroDomain genero = new GeneroDomain()
+                        {
+                            //Atribui a propriedade IdGenero o valor recebido no rdr
+                            IdGenero = Convert.ToInt32(rdr[0]),
+
+                            //Atribui a propriedade Nome o valor recebido no rdr
+                            Nome = rdr["Nome"].ToString()
+                        };
+
+                        //Adiciona cada objeto dentro da lista
+                        listaGeneros.Add(genero);
+                    }
+                }
+            }
+            //Retorna a lista de generos
+            return listaGeneros;
         }
     }
 }
