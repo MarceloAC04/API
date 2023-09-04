e8b1f997cbb68c8fac060f5bb1c0f322c224d86e
Marcelo Araujo <marcelocardoso020304@gmail.com>
2023-09-04 10:48:32 -0300

    criado o token

--- projeto/webapi.filme.manha/Controllers/UsuarioController.cs
+++ projeto/webapi.filme.manha/Controllers/UsuarioController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
 using webapi.filme.manha.Domains;
 using webapi.filme.manha.Interfaces;
 using webapi.filme.manha.Repositories;
@@ -19,6 +23,7 @@ namespace webapi.filme.manha.Controllers
     public class UsuarioController : ControllerBase
     {
         private IUsuarioRepository _usuarioRepository { get; set; }
+      
 
         public UsuarioController()
         {
@@ -41,11 +46,53 @@ namespace webapi.filme.manha.Controllers
 
                 if (usuarioEncontrado == null)
                 {
-                    return NotFound("Nenhum usuário foi encontrado");
+                    return NotFound("Usuário ou Senha Inválidos");
                 }
 
-                //Retorna o objeto no formato JSON com o status code Ok(200)
-                return Ok(usuarioEncontrado);
+                //Caso encontre o usuário, prossegue para criação do token
+
+                //1) - Defini as informações(claims) que serão fornecidas no token(PAYLOAD)
+                var claims = new[]
+                {
+                    new Claim(JwtRegistredClaimNames.Jti, usuarioEncontrado.IdUsuario.ToString()), 
+                    new Claim(JwtRegistredClaimNames.Email, usuarioEncontrado.Email),
+                    new Claim(ClaimTypes.Role, usuarioEncontrado.Permissao.ToString()),
+
+
+                    //Existe a possibilidade de criar uma claim personalizada
+                    new Claim("Claim personalizada", "Valor da Claim personalizada")
+                };
+
+                //2) - Defini a chave de acesso ao Token
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("filmes-chave-autenticacao-webapi-dev"));
+
+
+                //3) - Defini as credenciais do token (HEADER)
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                //4) - Gerar o token 
+                var token = new JwtSecurityToken
+                (
+                    //emissor do token
+                    issuer: "webapi.filme.manha",
+
+                    //Destinatario de token
+                    audience: "webapi.filme.manha",
+
+                    //dados definidos nas claims(Informações)
+                    claims: claims,
+
+                    //tempo de expiração de token
+                    expires: DateTime.Now.AddMinutes(5),
+
+                    //credencias do token
+                    signingCredentials: creds
+                );
+
+                //5) - Retorna o token criado
+                return Ok( new {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
 
             }
             catch (Exception erro)
--- projeto/webapi.filme.manha/Domains/UsuarioDomain.cs
+++ projeto/webapi.filme.manha/Domains/UsuarioDomain.cs
@@ -9,6 +9,7 @@ namespace webapi.filme.manha.Domains
         [Required(ErrorMessage = "O email do usuário é obrigatório!")]
         public string Email { get; set; }
 
+        [StringLength(20, MinimumLength = 4, ErrorMessage ="A senha dever ter de 4 a 20 caracteres")]
         [Required(ErrorMessage = "A senha do usuário é obrigatório!")]
         public string Senha { get; set; }
 
